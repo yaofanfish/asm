c43d0e3d73976c51a68e4aeaad4b0fa1ab16a0c6
yaofanfish <yaofanfish@gmail.com>
2026-02-02 00:45:27 +0000

    commit

--- push.py
+++ push.py
@@ -1,17 +1,25 @@
-import sys, os, subprocess, time
+import sys, os, subprocess, time, json
 
 PUSH_PERIOD=5 # seconds
 MAX_PUSH=5 # times
 VERBOSE=0
 PUSH_FILE="pu.sh"
+JSON_FILE="push.json"
 
 pu_sh_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), PUSH_FILE)
+jason_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), JSON_FILE)
+def upd(i=None):
+	if i is None:
+		i = globals()["i"]
+	with open(jason_file, "w") as f:
+		json.dump({"pushno": i}, f)
 i=0
 if not MAX_PUSH:
 	sys.exit()
 while 1:
 	print("\033[1;36mPUSH\033[0m") if VERBOSE else None
 	subprocess.run([pu_sh_file], capture_output=not bool(VERBOSE))
+	upd()
 	i+=1
 	if MAX_PUSH!=-1:
 		if i>=MAX_PUSH:
