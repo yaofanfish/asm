06be18908c4ffbdeb3925dd2a4ec37666259ff4d
yaofanfish <yaofanfish@gmail.com>
2026-02-02 00:40:34 +0000

    commit

--- push.py
+++ push.py
@@ -10,11 +10,11 @@ i=0
 if not MAX_PUSH:
 	sys.exit()
 while 1:
-	print("PUSH") if VERBOSE else None
+	print("\033[1;36mPUSH\033[0m") if VERBOSE else None
 	subprocess.run([pu_sh_file], capture_output=not bool(VERBOSE))
 	i+=1
 	if MAX_PUSH!=-1:
 		if i>=MAX_PUSH:
-			print("STOPPED AUTO-PUSHING") if VERBOSE else None
+			print("\033[1;31mSTOPPED AUTO-PUSHING\033[0m") if VERBOSE else None
 			sys.exit()
 	time.sleep(PUSH_PERIOD)
