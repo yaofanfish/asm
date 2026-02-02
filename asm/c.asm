1a8c3daef2be66671c40b1920e827dcee6353291
yaofanfish <yaofanfish@gmail.com>
2026-02-02 18:56:56 +0000

    commit

--- asm/c.asm
+++ asm/c.asm
@@ -1,19 +1,21 @@
+extern printf
 %macro syscall 0
-  int 0x80
+	int 0x80
 %endmacro
 section .data
-hi db "Hello, C"
+hi db "Hello, C! 1+1=%d", 10, 0
 hilen equ $-hi
 
 section .text
-global _start
-_start:
-  mov eax, 4
-  mov ebx, 1
-  mov ecx, hi
-  mov edx, hilen
-  syscall
+global main
+main:
+	mov edx, 1
+	add edx, 1
+	mov eax, 0
+	push edx
+	push hi		; "Hello, C! 1+1=%d"
+	call printf	; stack is 4 byte aligned
+	add esp, 8
 
-  mov eax, 1
-  mov ebx, 0
-  syscall
+	mov eax, 0
+	ret
