c8050fbdb0846cd80a4a96ed51ce10e555e65118
yaofanfish <yaofanfish@gmail.com>
2026-02-02 00:33:14 +0000

    commit

--- push.py
+++ push.py
@@ -7,11 +7,14 @@ PUSH_FILE="pu.sh"
 
 pu_sh_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), PUSH_FILE)
 i=0
+if not MAX_PUSH:
+	sys.exit()
 while 1:
+	print("PUSH") if VERBOSE else None
 	subprocess.run([pu_sh_file], capture_output=bool(VERBOSE))
 	i+=1
-	if i<=MAX_PUSH:
-		if MAX_PUSH!=-1:
-			print("STOPPED UPDATING") if VERBOSE else None
+	if MAX_PUSH!=-1:
+		if i>=MAX_PUSH:
+			print("STOPPED AUTO-PUSHING") if VERBOSE else None
 			sys.exit()
 	time.sleep(PUSH_PERIOD)
