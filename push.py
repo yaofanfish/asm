9bed92986661b72df1f8a8b0e41add61c52421d3
yaofanfish <yaofanfish@gmail.com>
2026-02-02 01:09:43 +0000

    commit

--- push.py
+++ push.py
@@ -1,25 +1,33 @@
+#!/usr/bin/python3
 import sys, os, subprocess, time, json
 
-MAX_PUSH_PERIOD=240 # seconds
-MAX_PUSH=5 # times
-VERBOSE=1
-PUSH_FILE="pu.sh"
-JSON_FILE="push.json"
+settings={}
+settings["MAX_PUSH_PERIOD"]=240 # seconds
+settings["MAX_PUSH"]=5 # times
+settings["VERBOSE"]=1
+settings["PUSH_FILE"]="pu.sh"
+settings["JSON_FILE"]="push.json"
 
-pu_sh_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), PUSH_FILE)
-jason_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), JSON_FILE)
+for i, j in enumerate(settings.keys()):
+	try:
+		settings[j]=sys.argv[i+1]
+	except IndexError:
+		break
+
+pu_sh_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), settings["PUSH_FILE"])
+jason_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), settings["JSON_FILE"])
 def checkstop(MAX_PUSH=None, i=None):
 	if MAX_PUSH==None:
-		MAX_PUSH = globals()["MAX_PUSH"]
+		MAX_PUSH = globals()["settings"]["MAX_PUSH"]
 	if i==None:
-		i = globals()["i"]
-	if MAX_PUSH!=-1:
-		if i>=MAX_PUSH:
+		i = globals()["settings"]["i"]
+	if settings["MAX_PUSH"]!=-1:
+		if i>=settings["MAX_PUSH"]:
 			print("\033[1;31mSTOPPED AUTO-PUSHING\033[0m")
 			sys.exit()
 def upd(i=None):
 	if i is None:
-		i = globals()["i"]
+		i = globals()["settings"]["i"]
 	with open(jason_file, "w") as f:
 		json.dump({"pushno": i, "stop": 0}, f)
 	with open(jason_file) as f:
@@ -30,13 +38,13 @@ def upd(i=None):
 	except KeyError:
 		pass
 i=0
-if not MAX_PUSH:
+if not settings["MAX_PUSH"]:
 	sys.exit()
 while 1:
-	print("\033[1;36mPUSH\033[0m") if VERBOSE else None
-	subprocess.run([pu_sh_file], capture_output=not bool(VERBOSE))
-	print("PUSHED") if VERBOSE else None
+	print("\033[1;36mPUSH\033[0m") if settings["VERBOSE"] else None
+	subprocess.run([pu_sh_file], capture_output=not bool(settings["VERBOSE"]))
+	print("PUSHED") if settings["VERBOSE"] else None
 	upd()
 	i+=1
-	checkstop(MAX_PUSH, i)
-	time.sleep(MAX_PUSH_PERIOD)
+	checkstop(settings["MAX_PUSH"], i)
+	time.sleep(settings["MAX_PUSH_PERIOD"])
